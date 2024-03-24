0902dc43bc35765a679c489a21a8e9812b0a6ea9
AndJHu97 <Andrewhu1997@gmail.com>
2024-03-24 17:13:03 -0400

    multipage and with react tree diagram

---
+++ frontend/src/Pages/VisualizeBackend.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Disease from '../VisualizationComponents/Disease'
+
+interface SymptomFormProps {}
+
+const SymptomForm: React.FC<SymptomFormProps> = () => {
+ 
+
+
+  return (
+   <Disease />
+  );
+};
+
+export default SymptomForm;
