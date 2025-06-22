8688e7b4b938c3087f2cf4e92e52cdd04b590175
Andrew <andrewhu1997@gmail.com>
2025-06-22 19:08:13 -0400

    changed allowed host for render

--- backend/MedDataDjango/settings.py
+++ backend/MedDataDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b7lt#l+!m21&9#382gr5f7(xruh5lfd4+abqm)shqbri9x40!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meddatabasev2.onrender.com']
 
 
 # Application definition
